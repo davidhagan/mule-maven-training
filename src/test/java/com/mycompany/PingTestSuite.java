e78a47bfc77c77f8ce14d7a1d6ca76f76e884181
COMSUPER\hagand <david.hagan@csc.gov.au>
2018-10-10 09:21:57 +1100

    master: added a failing test, so that jenkins can visibly fail

--- src/test/java/com/mycompany/PingTestSuite.java
+++ src/test/java/com/mycompany/PingTestSuite.java
@@ -1,5 +1,6 @@
 package com.mycompany;
 
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 import org.junit.Assert;
 import org.junit.Test;
@@ -8,6 +9,11 @@ public class PingTestSuite extends FunctionalTestCase {
 	@Test
 	public void testThatPingReturnsPong() throws Exception {
 		runFlowAndExpect("ping","pong");
+	}
+	@Test
+	public void failingTestThatPingReturnsPang() throws Exception {
+		MuleEvent event = runFlow("ping");
+		Assert.assertEquals("pang", event.getMessageAsString());
 	}
 	@Override
 	protected String getConfigFile(){
